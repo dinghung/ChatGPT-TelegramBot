97cb8e8cd9aa72a13670127a532a68a90f9499bd
dinghung <hung12077373@qq.com>
2023-03-09 10:03:36 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 git pull
 mvn clean install
+docker rmi harbor.test-tokencan.com/exchange/chatgpt-root
 docker build -t chatgpt-root .
 docker tag chatgpt-root:latest harbor.test-tokencan.com/exchange/chatgpt-root:latest
 docker push harbor.test-tokencan.com/exchange/chatgpt-root:latest
